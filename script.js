a52bf2e7c57543320252be1755fcd1f7834ba882
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2025-12-31 13:42:37 +0700

    feat: change cell to black on mouseover

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ row.className = "row";
 const cell = document.createElement("div");
 cell.className = "cell";
 
+
 for (let i = 0; i < gridDimension; i++){
     const newRow = row.cloneNode(true);
     for (let j = 0; j < gridDimension ; j++){
         const _ = cell.cloneNode(true);
         _.innerText = `${i},${j}`;
+        _.addEventListener("mouseover", function (e){
+            this.style["background-color"]="black";
+        });
         newRow.appendChild(_);
     }
     container.appendChild(newRow);
