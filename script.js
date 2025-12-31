1c4456c46fd57c462b56bb969163feb493693466
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2025-12-31 12:07:01 +0700

    feat: create rows and divs for grid square

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 container.id = "container";
 body.appendChild(container);
-container.innerText = "Hello world!";
+
+const gridDimension = 16;
+const cell = document.createElement("div");
+cell.class = "cell";
+for (let i = 0; i < gridDimension; i++){
+    const row = document.createElement("div");
+    row.class = "row";
+    for (let j = 0; j < gridDimension ; j++){
+            const _ = cell.cloneNode(true);
+        _.innerText = `${i},${j}`;
+        row.appendChild(_);
+    }
+    container.appendChild(row);
+}
