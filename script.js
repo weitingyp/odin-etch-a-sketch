012747bf8f5531f77df965d631aa657b1ea67d69
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2025-12-31 12:14:06 +0700

    refactor: clone newRows from Row node

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ container.style["flex-direction"] = "column";
 body.appendChild(container);
 
 const gridDimension = 16;
+
+const row = document.createElement("div");
+row.class = "row";
+row.style["display"] = "flex";
+row.style["flex-direction"] = "row";
+
 const cell = document.createElement("div");
 cell.class = "cell";
+
 for (let i = 0; i < gridDimension; i++){
-    const row = document.createElement("div");
-    row.class = "row";
-    row.style["display"] = "flex";
-    row.style["flex-direction"] = "row";
+    const newRow = row.cloneNode(true);
     for (let j = 0; j < gridDimension ; j++){
         const _ = cell.cloneNode(true);
         _.innerText = `${i},${j}`;
-        row.appendChild(_);
+        newRow.appendChild(_);
     }
-    container.appendChild(row);
+    container.appendChild(newRow);
 }
