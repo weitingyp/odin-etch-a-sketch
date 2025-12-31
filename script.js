214158db42a74666d747e48fc0030d41f53463e2
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2025-12-31 12:12:27 +0700

    feat: add flexbox display and direction to grid

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const body = document.querySelector("body");
 
 const container = document.createElement("div");
 container.id = "container";
+container.style["display"] = "flex";
+container.style["flex-direction"] = "column";
 body.appendChild(container);
 
 const gridDimension = 16;
@@ -10,6 +12,8 @@ cell.class = "cell";
 for (let i = 0; i < gridDimension; i++){
     const row = document.createElement("div");
     row.class = "row";
+    row.style["display"] = "flex";
+    row.style["flex-direction"] = "row";
     for (let j = 0; j < gridDimension ; j++){
         const _ = cell.cloneNode(true);
         _.innerText = `${i},${j}`;
